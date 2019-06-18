4d2f74219ec80c2ca8d83aeb1d708cd16699aa4c
juan asis <juan.asis1234gmail.com>
2019-06-18 17:24:56 -0300

    Test Cliente menor de edad atemporal

--- sistema-cuenta-bancaria/src/test/java/ar/edu/undec/entidades/TestUnitCliente.java
+++ sistema-cuenta-bancaria/src/test/java/ar/edu/undec/entidades/TestUnitCliente.java
@@ -4,23 +4,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
+import ar.edu.undec.Exception.ClienteException;
 import ar.edu.undec.Exception.ClienteIncompletoException;
-
+import ar.edu.undec.Exception.ClienteMenorDeEdadException;
+import ar.edu.undec.repositorio.IDateTime;
+import ar.edu.undec.sistema_cuenta_bancaria.MockitoExtension;
+@ExtendWith(MockitoExtension.class)
 class TestUnitCliente {
-
+	
+	
+	@Mock
+	IDateTime fechaDelSistema;
+	
+	
+		
 	@Test
-	public void factoryCliente_TodosLosAtributosOk_instanciaCorrecta() throws ClienteIncompletoException {
-		Cliente unCliente =Cliente.factoryCliente(1, "Juan Perez", "12345678", "San Martin 100","3825-124356",LocalDate.of(1990, 2, 01));
-		Assertions.assertNotNull(unCliente);
+	public void factoryCliente_TodosLosAtributosOk_instanciaCorrecta() throws ClienteException, ClienteIncompletoException {
+		LocalDate fechaDeNacimiento = LocalDate.of(1990, 12, 12);
+		Mockito.when(fechaDelSistema.getDate()).thenReturn(LocalDate.of(2019,06,06));
+		Cliente unCliente = Cliente.factoryCliente(fechaDelSistema,"Thor", "39473874", "3826363-323" , "Asgard", fechaDeNacimiento);
+		assertNotNull(unCliente);
 	
 	}
     @Test
-    public void factoryCliente_MenorDeEdad_ClienteIncompletoException(){
-        Assertions.assertThrows(ClienteIncompletoException.class, ()-> Cliente.factoryCliente(null,null,null,"Perez","123456",null));
-    }
+	public void factoryCliente_MenorDe18Anios_InstanciaIncompleta() {
+		LocalDate fechaNacimiento = LocalDate.of(2010, 12, 12);
+		Mockito.when(fechaDelSistema.getDate()).thenReturn(LocalDate.of(2019, 06, 6));
+		assertThrows(ClienteMenorDeEdadException.class, () -> Cliente.factoryCliente(fechaDelSistema,"Iron man", "20092019", "273832-233232" , "Tierra", fechaNacimiento));
+	}
+    @Test
+	public void factoryCliente_FaltanAtributos_InstanciaIncompleta() {
+		LocalDate fechaDeNacimiento = null;
+		Mockito.when(fechaDelSistema.getDate()).thenReturn(LocalDate.of(2019, 06, 6));		
+		assertThrows(ClienteIncompletoException.class, () -> Cliente.factoryCliente(fechaDelSistema,"", "", "" , "", fechaDeNacimiento));
+	}
 }
 
 
